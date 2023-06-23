f01a6d3e8702878b1bb2661944f757ff8c6651f4
Alexandr <sanekstroyka33@mail.ru>
2023-06-23 17:37:30 +0300

    home work

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿// Console.Write("Введите пятизначное число: ");
+// string number = Console.ReadLine()!;
+// if(number.Length == 5)
+// {
+//     if(number[0] == number[4] && number[1] == number[3])
+//     {
+//         Console.WriteLine($"Число является полиндромом");
+//     }
+//     else
+//     {
+//         Console.WriteLine($"Число не является полиндромом");
+//     }
+// }
+// else
+// {
+//     Console.WriteLine($"Введено не пятизначное число");
+// }
+// Console.Write("Введите координату y точки A: "); 
+// int ax = Convert.ToInt32(Console.ReadLine()!);  
+// Console.Write("Введите координату y точки A: "); 
+// int ay = Convert.ToInt32(Console.ReadLine()!); 
+// Console.Write("Введите координату y точки A: ");
+// int az = Convert.ToInt32(Console.ReadLine()!); 
+// Console.Write("Введите координату y точки B: "); 
+// int bx = Convert.ToInt32(Console.ReadLine()!);  
+// Console.Write("Введите координату y точки B: "); 
+// int by = Convert.ToInt32(Console.ReadLine()!); 
+// Console.Write("Введите координату y точки B: ");
+// int bz = Convert.ToInt32(Console.ReadLine()!); 
+// double AB = Math.Sqrt(Math.Pow((bx-ax), 2) + Math.Pow((by-ay), 2) + Math.Pow((bz-az), 2));
+// Console.WriteLine($"Растояние между AB = {AB:f2}"); 
+// Console.Write("Введите число: ");  
+// int number = int.Parse(Console.ReadLine()!);  
+// for (int i = 1; i <= number; i++) 
+// {     
+//     int sqr = Convert.ToInt32(Math.Pow(i, 3));     
+// Console.WriteLine(sqr);
+//  }
